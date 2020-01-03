77dbeb4b293222ec5e57eb5da2f82745288d74c4
junhyung9985 <junhyung9985@github.com>
2020-01-03 21:50:05 +0900

    ?

---
+++ test_image.py
@@ -0,0 +1,54 @@
+import os
+from os import walk
+from os.path import isfile, join
+from os import listdir
+import glob
+
+train_path = "../Hanguel_Database/Image_train"
+test_path = "../Hanguel_Database/Image_test"
+#ts = "/content/drive/My Drive/DeepLearning_group3/Hanguel_Database/Image_test"
+
+def findFiles(path):
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    return onlyfiles
+
+test_file = findFiles(test_path)
+train_file = findFiles(train_path)
+
+from PIL import Image
+import numpy as np
+import csv
+
+! cd "../Hanguel_Database/" && mkdir test && mkdir train
+
+test_dir = "../Hanguel_Database/test"
+train_dir = "../Hanguel_Database/train"
+
+label = open('label.csv', 'r', encoding='utf-8')
+rdr = csv.reader(label)
+for line in rdr:
+    save_ts = test_dir+"/"+line
+    save_tr = train_dir+"/"+line
+    if (!os.path.exists(save_ts)): os.makedirs(os.path.join(save_ts))
+    if (!os.path.exists(save_tr)): os.makedirs(os.path.join(save_tr))
+
+for f in test_file:
+    f = f.split('_')
+    class_name = f[0]
+    num = (f[1].split('.'))[0]
+    ls = list(csv.reader(open(join(test_path, f), newline=''), delimiter=','))
+    lst = []
+    for tmp in ls:
+        temp = [a for a in tmp if a!='']
+        lst.append(temp)
+    print(lst)
+    matrix = np.array(lst).astype("uint8")
+    imgObj = Image.fromarray(matrix)
+    resized_imgObj = imgObj.resize((28, 28))
+    resized_imgObj.save("../Hanguel_Database/test/{}/{}.jpg".format(class_name,num))
+    '''
+    imgObj.show()
+    resized_imgObj.show()
+    resized_matrix = np.asarray(resized_imgObj)
+    np.savetxt(test_dir+'/resized.csv', resized_matrix, fmt='%3.0d', delimiter=',')
+    '''
