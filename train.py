73befeee54887d942ae8171a7f8cc4933d28c6bb
junhyung9985 <junhyung9985@github.com>
2020-01-05 18:59:58 +0900

    Final

--- train.py
+++ train.py
@@ -8,9 +8,100 @@ from torchvision import datasets, models, transforms
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import os
+from PIL import Image
+import csv
+from os import walk
+from os.path import isfile, join
+from os import listdir
+import glob
 import warnings
 warnings.filterwarnings("ignore")
 
+'''
+
+train_path = "../Hanguel_Database/Image_train"
+test_path = "../Hanguel_Database/Image_test"
+#ts = "/content/drive/My Drive/DeepLearning_group3/Hanguel_Database/Image_test"
+
+def findFiles(path):
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    return onlyfiles
+
+test_file = findFiles(test_path)
+train_file = findFiles(train_path)
+
+
+test_dir = "../Hanguel_Database/test"
+train_dir = "../Hanguel_Database/train"
+
+label = open('labels.csv', 'r', encoding='utf-8')
+rdr = csv.reader(label)
+for line in rdr:
+    line = line[0]
+    save_ts = test_dir+"/"+line
+    save_tr = train_dir+"/"+line
+    if (os.path.exists(save_ts)==False): os.makedirs(os.path.join(save_ts))
+    if (os.path.exists(save_tr)==False): os.makedirs(os.path.join(save_tr))
+
+for f in train_file:
+    fi = f.split('_')
+    class_name = fi[0]
+    num = (fi[1].split('.'))[0]
+    ls = list(csv.reader(open(join(train_path, f), newline=''), delimiter=','))
+    lst = []
+    for tmp in ls:
+        temp = [a for a in tmp if a!='']
+        lst.append(temp)
+    print(lst)
+    matrix = np.array(lst).astype("uint8")
+    imgObj = Image.fromarray(matrix)
+    resized_imgObj = imgObj.resize((28, 28))
+    resized_imgObj.save("../Hanguel_Database/train/{}/{}.jpg".format(class_name,num))
+'''
+# Training_set을 ImageLoader와 호환할 수 있게 해주는 코드
+'''
+
+train_path = "../Hanguel_Database/Image_train"
+test_path = "../Hanguel_Database/Image_test"
+#ts = "/content/drive/My Drive/DeepLearning_group3/Hanguel_Database/Image_test"
+
+def findFiles(path):
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    return onlyfiles
+
+test_file = findFiles(test_path)
+train_file = findFiles(train_path)
+
+test_dir = "../Hanguel_Database/test"
+train_dir = "../Hanguel_Database/train"
+
+label = open('labels.csv', 'r', encoding='utf-8')
+rdr = csv.reader(label)
+for line in rdr:
+    line = line[0]
+    save_ts = test_dir+"/"+line
+    save_tr = train_dir+"/"+line
+    if (os.path.exists(save_ts)==False): os.makedirs(os.path.join(save_ts))
+    if (os.path.exists(save_tr)==False): os.makedirs(os.path.join(save_tr))
+
+for f in test_file:
+    fi = f.split('_')
+    class_name = fi[0]
+    num = (fi[1].split('.'))[0]
+    ls = list(csv.reader(open(join(test_path, f), newline=''), delimiter=','))
+    lst = []
+    for tmp in ls:
+        temp = [a for a in tmp if a!='']
+        lst.append(temp)
+    print(lst)
+    matrix = np.array(lst).astype("uint8")
+    imgObj = Image.fromarray(matrix)
+    resized_imgObj = imgObj.resize((28, 28))
+    resized_imgObj.save("../Hanguel_Database/test/{}/{}.jpg".format(class_name,num))
+
+
+'''
+# Test_set을 ImageLoader와 호환할 수 있게 해주는 코드
 
 class MyCNN(nn.Module):
     def __init__(self, output_dim=10):
