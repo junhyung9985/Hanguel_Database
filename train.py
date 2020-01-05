1aea37a685e4ee75a1bd82742f2920cb86c1a710
junhyung9985 <junhyung9985@github.com>
2020-01-05 16:27:02 +0900

    deleted some extra files

--- train.py
+++ train.py
@@ -19,13 +19,13 @@ class MyCNN(nn.Module):
         self.output_dim=output_dim
 
         self.cnn_layers = nn.Sequential(
-            nn.Conv2d(3,32,3,padding=1), # try with different kernels
+            nn.Conv2d(3,32,3,padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32,32,3,padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2), # 32 x (25x25)
+            nn.MaxPool2d(2,2), # 32 x (14x14)
             
             nn.Conv2d(32,16,3,padding=1),
             nn.BatchNorm2d(16),
@@ -33,7 +33,7 @@ class MyCNN(nn.Module):
             nn.Conv2d(16,16,3,padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2) # 16 x (5x5) 
+            nn.MaxPool2d(2,2) # 16 x (7x7)
         )
         self.fc_layer = nn.Sequential(
             nn.Linear(16*7*7,100),
@@ -49,7 +49,6 @@ class MyCNN(nn.Module):
 
         return out
 
-"""# try to change the learning rate"""
 
 learning_rate = 0.001
 output_dim=2350
@@ -61,27 +60,23 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 param_list = list(model.children())
 print(param_list)
 
-"""# try to change transform function
-* https://pytorch.org/docs/stable/torchvision/transforms.html
-"""
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize((28, 28)),
+        transforms.Resize(28,28),
         transforms.ToTensor(),
-        #transforms.Normalize([0.5, 0.5, 0.5], [0.3, 0.3, 0.3])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.3, 0.3, 0.3])
     ]),
     'test': transforms.Compose([
-        transforms.Resize((28, 28)),
+        transforms.Resize(28,28),
         transforms.ToTensor(),
-        #transforms.Normalize([0.5, 0.5, 0.5], [0.3, 0.3, 0.3])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.3, 0.3, 0.3])
     ]),
 }
 test_transform = data_transforms['test']
 
-"""**data loader**"""
 
-batch_size = 64 # try with different batch_size
+batch_size = 64
 test_batch_size = 10
 data_dir = '/home/junhyung9985/Hanguel_Database/'
 train_dir = 'train'
@@ -101,10 +96,10 @@ class_names = train_set.classes
 
 print(class_names) 
 
-"""**training**"""
+
 
 result_dir = '/home/junhyung9985/Hanguel_Database/result'
-num_epoch = 1 # try with different epochs and find the best epoch
+num_epoch = 20
 
 if not os.path.exists(result_dir):
     os.mkdir(result_dir)    
@@ -125,19 +120,5 @@ for i in range(num_epoch):
             print(i,j, loss.data.cpu())
 
 print('training is done by max_epochs', num_epoch)
+torch.save(model, result_dir + '/epoch_{}_lr_{}.model'.format(num_epoch, learning_rate))
 
-model.eval()
-hits = 0
-for k,[image,label] in enumerate(test_loader):
-    x = image.cuda()
-    y_= label.cuda()
-  
-    output = model(x)
-    y_est = output.argmax(1)
-    print('Target', label.numpy(), 'Prediction', y_est.cpu().numpy())
-    hits = hits + sum(y_est == y_)
-print('hits, accuracy', hits, hits/(len(test_set)+0.0))
-
-
-torch.save(model, result_dir + '/ACC_{}_{}.model'.format(num_epoch, hits/(len(test_set)+0.0)))
-#torch.save(test_transform, result_dir + 'teamX.transform')
